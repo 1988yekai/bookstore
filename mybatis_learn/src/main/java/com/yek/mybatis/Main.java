4a168a9c9190cdb0a5357d66c5f6915048bc66ce
yek <1988yekai@163.com>
2018-07-25 22:05:53 +0800

    add logback config

--- mybatis_learn/src/main/java/com/yek/mybatis/Main.java
+++ mybatis_learn/src/main/java/com/yek/mybatis/Main.java
@@ -4,7 +4,14 @@ package com.yek.mybatis;
  * Created by yek on 2018-07-22.
  */
 public class Main {
+    private static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-
+        for (int i = 0; i < Integer.MAX_VALUE; i++){
+            logger.trace("trace: " + i);
+            logger.debug("debug: " + i);
+            logger.info("info: " + i);
+            logger.warn("warn: " + i);
+            logger.error("error: " + i);
+        }
     }
 }
